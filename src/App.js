daf0c41b139c1c2775b84ca7728ca9b18a7171f9
Emilio Depena <emiliosdep@gmail.com>
2020-04-14 19:31:14 -0400

    added css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React, { Component} from 'react';
 import './App.css';
-
+import Resource from './components/Resource';
+import resources from './mock/resources'
 class App extends Component {
+
+
+  // constructor(props) {
+  //   super(props);
+  //   this.renderPosts.bind(this)
+  // }
+
+  renderPosts() {
+    const display = resources.map((resource) => {
+      return <Resource resource={resource} />
+    })
+
+    return display;
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>Welcome to BrainHive!</h1>
+        <div className="header">
+        <h1 className="title">Welcome to BrainHive!</h1>
+        <div/>
+        <div classname="resourceList">{this.renderPosts()}<div/>
       </div>
     );
   }
