9c1891586d6a135ae784747c90580b913768fddf
Emilio Depena <emiliosdep@gmail.com>
2020-04-23 17:15:28 -0400

    finished fill out forms

--- src/App.js
+++ src/App.js
@@ -1,36 +1,47 @@
-import React, { Component} from 'react';
-import './App.css';
-import Resource from './components/Resource';
-import resources from './mock/resources'
+import React, { Component } from "react";
+import "./App.css";
+import Resource from "./components/Resource";
+import resources from "./mock/resources";
 
-import ResourceForm from './components/ResourceForm'
+import ResourceForm from "./components/ResourceForm";
+
 class App extends Component {
-
-
+  //[<Resource resource={aResource}/>, <Resource resource={aResource}]
   // constructor(props) {
   //   super(props);
-  //   this.renderPosts.bind(this)
+  //   this.renderPosts.bind(this);
   // }
+  // Resource = require("./components/Resource");
+  state = {
+    resources: [...resources],
+  };
 
-  renderPosts() {
-    const display = resources.map((resource) => {
-      return <Resource resource={resource} />
-    })
+  addResource = (newResource) => {
+    // this.state.resources.push(newResource);
+    this.setState({
+      resources: [...this.state.resources, newResource],
+    });
+  };
+
+  renderPosts = () => {
+    const display = this.state.resources.map((resource) => {
+      return <Resource resource={resource} key={resource.title} />;
+    });
 
     return display;
-  }
+  };
 
   render() {
     return (
       <div className="App">
         <div className="header">
-        <h1 className="title">Welcome to BrainHive!</h1>
-        <div className="navigation">
-          <a href='/'>Add Resource</a>
+          <h1 className="title">Welcome to BrainHive!</h1>
+          <div className="navigation">
+            <a href="">Add Resource</a>
+          </div>
         </div>
-        </div>
-        <div classname="resourceList">{this.renderPosts()}</div>
-        <ResourceForm/>
+        <div className="resourceList">{this.renderPosts()}</div>
+        <ResourceForm addResource={this.addResource} />
       </div>
     );
   }
