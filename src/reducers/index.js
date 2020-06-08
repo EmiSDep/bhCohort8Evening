b87bacbd3772e318a1654e5b2d35d1381496cea7
Emilio Depena <emiliosdep@gmail.com>
2020-06-07 22:12:01 -0400

    added store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,31 @@
 import {combineReducers} from 'redux';
 import resourcesReducer from './resourcesReducer';
 import newResourceReducer from './newResourceReducer'
+import searchReducer from './searchReducer'
 
 export default combineReducers({
     resources: resourcesReducer,
-    newResource: newResourceReducer
+    newResource: newResourceReducer,
+    search: searchReducer
 })
+
+const store = {
+    resources: {
+        list: [],
+        loading: true,
+        errors: {},
+        count: 0
+    },
+    newResource: {
+        form: {
+        },
+        loading: false,
+        errors: {},
+    },
+    search: {
+        list: [],
+        query: '',
+        loading: false,
+        errors: {}
+    }
+}
